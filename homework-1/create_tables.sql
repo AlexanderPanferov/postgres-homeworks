2ed2f5ceef4b700441a35f06c7d04054f914bad0
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-08-25 20:07:52 +0700

    add sql code

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers_data
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name text
+);
+CREATE TABLE employees_data
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+CREATE TABLE orders_data
+(
+	order_id char(5) PRIMARY KEY,
+	customer_id char(5) REFERENCES customers_data(customer_id) NOT NULL,
+	employee_id int REFERENCES employees_data(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100)
+);
